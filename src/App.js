b65047bb5ff364472a35f902d194cc21d3bf4fa8
mahdi jirofti <mahdi.jirofti12@gmail.com>
2017-12-28 11:31:42 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <p>WEB APP REACT ROUTER</p>
+      </div>
+    );
+  }
+}
+
+export default App;
