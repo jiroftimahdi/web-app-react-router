c483efdddb4e41453e5a0d633da4e22df9616993
mahdi jirofti <mahdi.jirofti12@gmail.com>
2017-12-30 12:06:50 +0330

    Implement Local history

--- src/History.js
+++ src/History.js
@@ -3,4 +3,39 @@ class History{
     this.history = [];
     this.index = -1;
   }
+  goBack(){
+    this.index--;
+    return history[this.index];
+  }
+  goForward(){
+    this.index++;
+    return history[this.index];
+  }
+  removeAllAhead(){
+    this.history = this.history.filter((item,index)=>{
+      return index<=this.index;
+    });
+  }
+  removeAllAback(){
+    this.history = this.history.filter((item,index)=>{
+      return index>=this.index;
+    });
+  }
+  removePageAt(index){
+    this.history = this.history.filter((item,_index)=>{
+      return _index !== index
+    })
+  }
+  pushPage(page){
+    this.history.splice(this.index+1,0,page);
+    this.index++;
+    return this.history[this.index];
+  }
+  popPage(){
+    this.history.splice(this.index,1);
+    this.index--;
+    return this.history[this.index];
+  }
 }
+
+export default History;
